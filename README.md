9f61f31b1b5c7a8339840ece5d0abf87aedc2b3e
zxj5470 <zxj5470@foxmail.com>
2018-07-09 17:43:01 +0800

    rename app

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# flutter_app
-
-A new Flutter application.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
+# Social Vertex Flutter
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Social Vertex Flutter',
       theme: new ThemeData(
         // This is the theme of your application.
         //
