f7215f51958f92dd0e2677a25a0aeb65c7c61fc7
zxj5470 <zxj5470@foxmail.com>
2018-07-09 17:49:40 +0800

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_app/main.dart';
+import 'package:social_vertex_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
