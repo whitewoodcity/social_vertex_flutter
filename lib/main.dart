4d69d4fd9e3c423045ce12d7b23609edca243527
zxj5470 <zxj5470@foxmail.com>
2018-07-09 17:57:42 +0800

    update app_name

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Social Vertex Flutter'),
     );
   }
 }
