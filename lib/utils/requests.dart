5389689199dd1eb7735985805a923011a4156e47
zxj5470 <zxj5470@foxmail.com>
2018-07-13 02:23:02 +0800

    add ui_specify and MD5

--- lib/utils/requests.dart
+++ lib/utils/requests.dart
@@ -1,4 +1,15 @@
 import 'dart:io';
+import 'dart:convert';
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart' as crypto;
+
+String generateMd5(String data) {
+  var content = new Utf8Encoder().convert(data);
+  var md5 = crypto.md5;
+  var digest = md5.convert(content);
+  var password = hex.encode(digest.bytes);
+  return password;
+}
 
 void socketOperator(Socket socket, Function f) {
   socket.write(
