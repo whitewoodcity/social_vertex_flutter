afb48c6654fe8450a7736b846a39275356d2d992
GZYangKui <752544765@qq.com>
2018-07-13 10:09:16 +0800

    Fix Bug

--- lib/utils/newuser_info.dart
+++ lib/utils/newuser_info.dart
@@ -1,7 +1,11 @@
 class NewUser{
-  var _userName;
-  var _password;
+  String _userName;
+  String _password;
 
-  NewUser(this._userName, this._password);
-
+  NewUser(this._userName,this._password);
+  Map<String, dynamic> toJson() =>
+      {
+        'userName': _userName,
+        'password': _password,
+      };
 }
