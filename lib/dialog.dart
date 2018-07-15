3042e37930998e2ab0498b43c626c42da074e4df
GZYangKui <752544765@qq.com>
2018-07-15 22:42:31 +0800

    FixBug

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -1,61 +1,57 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:social_vertex_flutter/component/message_Item.dart';
-import 'package:social_vertex_flutter/user.dart';
+import 'package:social_vertex_flutter/component/message_item.dart';
 
-var _targetName;
+var _name;
 class DialogStateful extends StatefulWidget {
-
   @override
   DialogState createState() => new DialogState();
 
-  DialogStateful(targetName){
-    _targetName=targetName;
+  DialogStateful(name){
+    _name=name;
   }
+
 }
 
 class DialogState extends State<DialogStateful> {
   var _message = "";
-  var _receiveMessage;
-  var _source = "own";
-  List<Widget> _list = [];
+  List<MessageEntry> _list = new List();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("${_targetName}"),
-        centerTitle: true,
-      ),
-      body: new ListView(
-        children: _list,
-      ),
-      bottomNavigationBar: new BottomAppBar(
-        child: new Row(
-          children: <Widget>[
-            new Expanded(child: new TextField(
-              onChanged: (value) {
-                _message = value;
-              },
-            )),
-            new RaisedButton(
-              onPressed: _sendMessage,
-              child: new Text("发送"),
-            )
-          ],
+        appBar: new AppBar(
+          title: new Text(_name),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: new ListView(
+          children:_list,
+        ),
+        bottomNavigationBar: new BottomAppBar(
+          child: new Row(
+            children: <Widget>[
+              new Expanded(child: new TextField(
+                onChanged: (value) {
+                  _message = value;
+                },
+              )),
+              new RaisedButton(
+                onPressed: _sendMessage,
+                child: new Text("发送"),
+              )
+            ],
+          ),
+        ));
   }
 
   _sendMessage() {
-    update(new MessageEntry(_message, _source));
+    updata(new MessageEntry(_message));
   }
-
-  void update(MessageEntry entry) {
-    _list = new List.from(_list);
+ 
+  void updata(MessageEntry entry){
     setState(() {
-      _list.add(new MessageEntry(_message, _source));
+      _list=new List.from(_list);
+      _list.add(new MessageEntry(_message));
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
-import 'user.dart';
-import 'utils/keys.dart' as keys;
 
 void main() => runApp(LoginStateless()); //整个应用的入口
 
