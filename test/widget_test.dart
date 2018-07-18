4a8f5815c14224427050af95309a80b275e1a075
chengenzhao001 <chengen.zhao@mail.mcgill.ca>
2018-07-18 09:45:34 +0800

    add test file

---
+++ test/widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+
+    double i = 3.25;
+    double j = 2.125;
+
+    print((0 % j));
+
+  });
+}
