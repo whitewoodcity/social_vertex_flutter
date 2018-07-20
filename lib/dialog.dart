4dbf0b23895e73701c628b31a1d6a8d0a6215ad9
zxj5470 <zxj5470@foxmail.com>
2018-07-20 20:12:34 +0800

    input keyboard expand

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:social_vertex_flutter/component/message_item.dart';
 import 'main.dart';
 
-Widget showChatDialog(String name,MyHomePageState state,List<MessageEntry> list) {
+Widget showChatDialog(String name, MyHomePageState state, List<MessageEntry> list) {
   var _message = "";
   return new Scaffold(
     appBar: new AppBar(
@@ -18,36 +18,45 @@ Widget showChatDialog(String name,MyHomePageState state,List<MessageEntry> list)
           ),
           onPressed: () {
             state.updateUi(2);
-
           }),
     ),
-    body: new ListView(
-      children: list,
-    ),
-    bottomNavigationBar: new BottomAppBar(
-      child: new Row(
-        children: <Widget>[
-          new Expanded(child: new TextField(
-            onChanged: (value) {
-              _message = value;
-            },
-          )),
-          new RaisedButton(
-            child: new Text("发送"),
-            onPressed: () {
-              var message = '''{
+    body: new Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        new Expanded(
+          child: new ListView(
+            children: list,
+          ),
+        ),
+        new Container(
+          color: Colors.white,
+          padding: new EdgeInsets.all(10.0),
+          child: new Row(
+            children: <Widget>[
+              new Expanded(
+                child: new TextField(
+                  onChanged: (value) {
+                    _message = value;
+                  },
+                ),
+              ),
+              new RaisedButton(
+                child: new Text("发送"),
+                onPressed: () {
+                  var message = '''{
             "type":"message",
             "from":"${state.userName}",
             "to":"$name",
              "body":"$_message",
               "version":0.1
                 }''';
-              state.sendMessage(message);
-            },
-          )
-        ],
-      ),
+                  state.sendMessage(message);
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
     ),
   );
 }
-
