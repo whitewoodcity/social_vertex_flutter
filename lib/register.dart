e403e73f73feb3aa5e36ba497bdd721b5c549bf2
wyd <wyudi@qq.com>
2019-06-24 22:50:59 +0800

    国际化，补全

--- lib/register.dart
+++ lib/register.dart
@@ -2,26 +2,27 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'config/ui_variables.dart';
 import "config/constants.dart" as constants;
 import 'utils/util.dart' as util;
 
 class RegisterPage extends StatefulWidget {
+
   @override
   RegisterState createState() => RegisterState();
 }
 
 class RegisterState extends State<RegisterPage> {
+  String _userName = "";
+  String _password = "";
+  String _repassword = "";
+  String _nickname = "";
 
-  String _userName="";
-  String _password="";
-  String _repassword="";
-  String _nickname="";
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("用户注册"),
+        title: Text(uiVariables["register"]),
         centerTitle: true,
       ),
       body: ListView(
@@ -29,22 +30,22 @@ class RegisterState extends State<RegisterPage> {
           TextField(
             textAlign: TextAlign.start,
             onChanged: (value) => _userName = value,
-            decoration: InputDecoration(labelText: "用户名"),
+            decoration: InputDecoration(labelText: uiVariables["username"]),
           ),
           TextField(
             textAlign: TextAlign.start,
             obscureText: true,
             onChanged: (String value) => _password = value,
-            decoration: InputDecoration(labelText: "密码"),
+            decoration: InputDecoration(labelText: uiVariables["password"]),
           ),
           TextField(
             obscureText: true,
             onChanged: (String value) => _repassword = value,
-            decoration: InputDecoration(labelText: "确认密码"),
+            decoration: InputDecoration(labelText: uiVariables["pw_confirm"]),
           ),
           TextField(
             onChanged: (String value) => _nickname = value,
-            decoration: InputDecoration(labelText: "昵称"),
+            decoration: InputDecoration(labelText: uiVariables["nickname"]),
           ),
           SizedBox.fromSize(
             size: Size(0.0, 10.0),
@@ -56,13 +57,13 @@ class RegisterState extends State<RegisterPage> {
                   if (_password == _repassword) {
                     _register(context);
                   } else {
-                    _registerAlert("两次密码不匹配");
+                    _registerAlert(uiVariables["pw_not_match"]);
                   }
                 } else {
-                  _registerAlert("信息不完整");
+                  _registerAlert(uiVariables["info_incomplete"]);
                 }
               },
-              child: Text("注册"),
+              child: Text(uiVariables["register"]),
             ),
           ),
         ],
@@ -82,29 +83,33 @@ class RegisterState extends State<RegisterPage> {
     };
 
     put("${constants.protocol}${constants.server}/",
-            headers: {"Content-Type":"application/json"},
-            body: json.encode(info) + constants.end)
+            headers: {"Content-Type": "application/json"}, body: json.encode(info) + constants.end)
         .then((response) {
       if (response.statusCode == 200) {
         var result = json.decode(utf8.decode(response.bodyBytes));
         if (result[constants.register]) {
-          Navigator.popAndPushNamed(context, "/login",
-            result:{constants.id:_userName, constants.password: _password},
-            arguments: {constants.id:_userName, constants.password: util.md5(_password), constants.nickname: _nickname});
+          Navigator.popAndPushNamed(context, "/login", result: {
+            constants.id: _userName,
+            constants.password: _password
+          }, arguments: {
+            constants.id: _userName,
+            constants.password: util.md5(_password),
+            constants.nickname: _nickname
+          });
         } else {
           _registerAlert(result["info"]);
         }
       } else {
-        _registerAlert("服务器异常,请重试!");
+        _registerAlert(uiVariables["system_error"]);
       }
-    }).catchError((error)=> print(error));
+    }).catchError((error) => print(error));
   }
 
   void _registerAlert(String info) {
     showDialog(
       context: context,
       builder: (BuildContext context) => SimpleDialog(
-            title: Text("消息"),
+            title: Text(uiVariables["msg"]),
             children: <Widget>[
               Align(
                 alignment: Alignment.center,
@@ -119,7 +124,7 @@ class RegisterState extends State<RegisterPage> {
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text("确定"),
+                  child: Text(uiVariables["ok"]),
                 ),
               ),
             ],
